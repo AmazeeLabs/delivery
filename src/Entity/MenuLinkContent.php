3e038d1ddc5ccc63b77532076ed60a3eba999381
Philipp Melab <philipp.melab@gmail.com>
2019-08-14 16:35:36 +0200

    Fixed case when there is no associated workspace for a menu item yet.

--- src/Entity/MenuLinkContent.php
+++ src/Entity/MenuLinkContent.php
@@ -9,7 +9,7 @@ class MenuLinkContent extends \Drupal\menu_link_content\Entity\MenuLinkContent {
   public function postSave(EntityStorageInterface $storage, $update = TRUE) {
     // Make sure to associate before running postSave so workspace associations
     // are up to date before the menu tree rebuild.
-    \Drupal::service('workspaces.association')->trackEntity($this, $this->workspace->entity);
+    \Drupal::service('workspaces.association')->trackEntity($this, $this->workspace->entity ?: \Drupal::service('workspaces.manager')->activeWorkspace());
     return parent::postSave($storage, $update);
   }
 
