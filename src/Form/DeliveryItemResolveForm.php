e875df63bd35a4ba1fa61382833a1702b9955b8a
Jamie Hollern <jamiehollern@users.noreply.github.com>
2020-02-25 08:37:44 +0000

    Added data lang to resolution editor instance.

--- src/Form/DeliveryItemResolveForm.php
+++ src/Form/DeliveryItemResolveForm.php
@@ -326,6 +326,13 @@ class DeliveryItemResolveForm extends FormBase {
             else if ($formDisplay->getComponent($property)) {
               $formElement = $customForm[$property];
               $formElement['widget']['#parents'] = ['custom', $languageId, $property];
+              // Add a language attribute to each editor widget.
+              foreach ($formElement['widget'] as $key => $widget) {
+                if (!is_numeric($key)) {
+                  continue;
+                }
+                $formElement['widget'][$key]['html']['#attributes'] = ['data-lang' => $languageId];
+              }
               $form[$languageId][$property]['selection']['#type'] = 'value';
               $form[$languageId][$property]['selection']['#value'] = '__custom__';
               $form[$languageId][$property]['preview'] = [
