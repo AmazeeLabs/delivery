521ff22542d444f01665114c39be7853771d727b
Jamie Hollern <jamiehollern@users.noreply.github.com>
2020-03-11 19:51:26 +0000

    Made link output workspace dependent.

--- src/Plugin/views/field/DeliveryItemUrl.php
+++ src/Plugin/views/field/DeliveryItemUrl.php
@@ -6,6 +6,8 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\views\ResultRow;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\workspaces\WorkspaceAssociationInterface;
+use Drupal\workspaces\WorkspaceManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -22,24 +24,57 @@ class DeliveryItemUrl extends FieldPluginBase implements ContainerFactoryPluginI
    */
   protected $entityTypeManager;
 
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $workspaceManager;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $workspaceAssociation;
+
+  /**
+   * {@inheritdoc}
+   */
   public static function create(
     ContainerInterface $container,
     array $configuration,
     $plugin_id,
     $plugin_definition
   ) {
-    return new static($configuration, $plugin_id, $plugin_definition, $container->get('entity_type.manager'));
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('workspaces.manager'),
+      $container->get('workspaces.association')
+    );
   }
 
-
+  /**
+   * DeliveryItemUrl constructor.
+   *
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param $plugin_definition
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   * @param \Drupal\workspaces\WorkspaceManagerInterface $workspaceManager
+   * @param \Drupal\workspaces\WorkspaceAssociationInterface $workspaceAssociation
+   */
   public function __construct(
     array $configuration,
     string $plugin_id,
     $plugin_definition,
-    EntityTypeManagerInterface $entityTypeManager
+    EntityTypeManagerInterface $entityTypeManager,
+    WorkspaceManagerInterface $workspaceManager,
+    WorkspaceAssociationInterface $workspaceAssociation
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entityTypeManager;
+    $this->workspaceManager = $workspaceManager;
+    $this->workspaceAssociation = $workspaceAssociation;
   }
 
 
@@ -55,9 +90,17 @@ class DeliveryItemUrl extends FieldPluginBase implements ContainerFactoryPluginI
    * {@inheritdoc}
    */
   public function getValue(ResultRow $values, $field = NULL) {
-    $entity = $this->entityTypeManager->getStorage($values->{$this->aliases['entity_type']})->loadRevision($values->{$this->field_alias});
+    // If there is no entity.
+    $entity = $this->entityTypeManager->getStorage($values->{$this->aliases['entity_type']})
+      ->loadRevision($values->{$this->field_alias});
     if (!$entity) {
-      return $this->t('Corresponding content not found');
+      return '';
+    }
+    // If this entity is not available in the current workspace.
+    $current_workspace_id = $this->workspaceManager->getActiveWorkspace()->id();
+    $ids = $this->workspaceAssociation->getEntityTrackingWorkspaceIds($entity);
+    if (!in_array($current_workspace_id, $ids)) {
+      return '';
     }
     return $entity->toLink()->getUrl()->toString();
   }
