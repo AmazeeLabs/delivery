fd7376037057aa55008810d7d357cea566764f95
Jamie Hollern <jamiehollern@gmail.com>
2020-09-03 09:07:55 +0100

    Tidy Views filter plugin.

--- src/Plugin/views/filter/WorkspacesList.php
+++ src/Plugin/views/filter/WorkspacesList.php
@@ -17,80 +17,82 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class WorkspacesList extends InOperator {
 
-  /**
-   * The entity type manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
+    /**
+     * The entity type manager service.
+     *
+     * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+     */
+    protected $entityTypeManager;
 
-  /**
-   * WorkspacesList constructor.
-   *
-   * @param array $configuration
-   * @param string $plugin_id
-   * @param \Drupal\delivery\Plugin\views\filter\mixed $plugin_definition
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   */
-  public function __construct(array $configuration, string $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->entityTypeManager = $entity_type_manager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
-    parent::init($view, $display, $options);
-    $this->valueTitle = t('Allowed workspaces');
-    $this->definition['options callback'] = [$this, 'generateOptions'];
-  }
-
-  /**
-   * Override the query so that no filtering takes place if the user doesn't
-   * select any options.
-   */
-  public function query() {
-    if (!empty($this->value)) {
-      parent::query();
-    }
-  }
-
-  /**
-   * Skip validation if no options have been chosen so we can use it as a
-   * non-filter.
-   */
-  public function validate() {
-    if (!empty($this->value)) {
-      parent::validate();
-    }
-  }
-
-  /**
-   * Helper function that generates the options.
-   *
-   * @return array
-   */
-  public function generateOptions() {
-    $workspaces = $this->entityTypeManager->getStorage('workspace')->loadMultiple();
-    $options = [];
-    foreach ($workspaces as $workspace) {
-      $options[$workspace->id()] = $workspace->label();
+    /**
+     * WorkspacesList constructor.
+     *
+     * @param array $configuration
+     * @param string $plugin_id
+     * @param \Drupal\delivery\Plugin\views\filter\mixed $plugin_definition
+     * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+     */
+    public function __construct(array $configuration, string $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+        parent::__construct($configuration, $plugin_id, $plugin_definition);
+        $this->entityTypeManager = $entity_type_manager;
     }
 
-    return $options;
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+        return new static(
+            $configuration,
+            $plugin_id,
+            $plugin_definition,
+            $container->get('entity_type.manager')
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+        parent::init($view, $display, $options);
+        $this->valueTitle = t('Allowed workspaces');
+        $this->definition['options callback'] = [$this, 'generateOptions'];
+    }
+
+    /**
+     * Override the query so that no filtering takes place if the user doesn't
+     * select any options.
+     */
+    public function query() {
+        if (!empty($this->value)) {
+            parent::query();
+        }
+    }
+
+    /**
+     * Skip validation if no options have been chosen so we can use it as a
+     * non-filter.
+     */
+    public function validate() {
+        if (!empty($this->value)) {
+            parent::validate();
+        }
+    }
+
+    /**
+     * Helper function that generates the options.
+     *
+     * @return array
+     * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+     * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+     */
+    public function generateOptions() {
+        $workspaces = $this->entityTypeManager->getStorage('workspace')->loadMultiple();
+        $options = [];
+        foreach ($workspaces as $workspace) {
+            $options[$workspace->id()] = $workspace->label();
+        }
+
+        return $options;
+    }
 
 }
