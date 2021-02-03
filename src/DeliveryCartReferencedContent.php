ef9b36d5b2ac69872c5245a56b73a27484f6c305
eli.stone <eli.stone@amazee.com>
2021-02-03 10:41:39 +0000

    Added the ability to find & add referenced blocks from layout builder.

--- src/DeliveryCartReferencedContent.php
+++ src/DeliveryCartReferencedContent.php
@@ -8,14 +8,19 @@ use Drupal\Core\Entity\EntityInterface;
 
 class DeliveryCartReferencedContent {
 
+  static $count = 0;
+
+
   /**
    * Looks for any referenced menu items and adds them to the cart.
    *
    * @param \Drupal\Core\Entity\EntityInterface $entity
+   *
+   * @return false|int
    */
   public static function addMenuItems(EntityInterface $entity){
     if(!self::isNode($entity)) {
-      return;
+      return FALSE;
     }
 
     $node_id = $entity->id();
@@ -30,10 +35,47 @@ class DeliveryCartReferencedContent {
           $linkEntity = MenuLinkContent::load($id);
           if($linkEntity){
             \Drupal::service('delivery.cart')->addToCart($linkEntity);
+            self::$count++;
           }
         }
       }
     }
+
+    return self::$count;
+  }
+
+  public static function addMediaItems(EntityInterface $entity) {
+
+  }
+
+  public static function addBlocksFromLayoutBuilder(EntityInterface $entity) {
+    if(!self::isNode($entity)) {
+      return FALSE;
+    }
+
+    if(!self::hasField($entity,'layout_builder__layout')) {
+      return FALSE;
+    }
+
+    $sections = $entity->get('layout_builder__layout')->getSections();
+
+    foreach ($sections as $section){
+      $components = $section->getComponents();
+      foreach ($components as $component){
+        $renderedArray = $component->toRenderArray();
+        $theme = $renderedArray['#theme'] ? $renderedArray['#theme'] : '';
+        if ($theme === "block") {
+          $content = $renderedArray['content'] ? $renderedArray['content'] : '';
+          if (isset($content['#block_content'])) {
+            $blockContent = $content['#block_content'];
+            \Drupal::service('delivery.cart')->addToCart($blockContent);
+            self::$count++;
+          }
+        }
+      }
+    }
+
+    return self::$count;
   }
 
   /**
@@ -43,8 +85,12 @@ class DeliveryCartReferencedContent {
    *
    * @return bool
    */
-  protected static function isNode($entity) {
+  protected static function isNode(EntityInterface $entity) {
     return $entity->getEntityTypeId() === "node";
+  }
+
+  protected static function hasField(EntityInterface $entity, string $field){
+    return $entity->hasField($field);
   }
 
 }
